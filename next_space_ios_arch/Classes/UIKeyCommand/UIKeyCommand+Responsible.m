2d7099ac7ee173c0783d6746fb41db4a64a634c7
mobileflowus <mobile@flowus.cn>
2022-10-21 20:35:09 +0800

    feat:增加快捷键分发框架完善信息 补充文档

--- next_space_ios_arch/Classes/UIKeyCommand/UIKeyCommand+Responsible.m
+++ next_space_ios_arch/Classes/UIKeyCommand/UIKeyCommand+Responsible.m
@@ -9,15 +9,41 @@
 
 @implementation UIKeyCommand(Responsible)
 
+/**
+ UIResponder 子类只需要声明这个方法即可
+ 
+ 递归式分发键盘快捷键事件 业务请复写,返回YES 将响应链自动断掉
+-(BOOL)onKeyCommand:(UIKeyCommand *)command commandEvent:(NSString*)event{
+   NSLog(@"===========>执行key %@ by %@  %@",command.input,self,event);
+   return YES;
+}
+ */
 + (instancetype)dispatchCommandWithTitle:(NSString *)title image:(UIImage *)image input:(NSString *)input modifierFlags:(UIKeyModifierFlags)modifierFlags  commandEvent:(NSString *) event{
     return [UIKeyCommand commandWithTitle:title image:image action: [self getDefaulActionSelector] input:input modifierFlags:modifierFlags propertyList:[NSDictionary dictionaryWithObject:event forKey:UIKeyCommandKeyCommandEvent]];
 }
 
-
+/**
+ UIResponder 子类只需要声明这个方法即可
+ 
+ 递归式分发键盘快捷键事件 业务请复写,返回YES 将响应链自动断掉
+-(BOOL)onKeyCommand:(UIKeyCommand *)command commandEvent:(NSString*)event{
+   NSLog(@"===========>执行key %@ by %@  %@",command.input,self,event);
+   return YES;
+}
+ */
 + (instancetype)dispatchCommandWithTitle:(NSString *)title image:(UIImage *)image input:(NSString *)input modifierFlags:(UIKeyModifierFlags)modifierFlags alternates:(NSArray<UICommandAlternate *> *)alternates     commandEvent:(NSString *) event API_AVAILABLE(ios(13.0)){
     return [UIKeyCommand commandWithTitle:title image:image action: [self getDefaulActionSelector] input:input modifierFlags:modifierFlags propertyList:[NSDictionary dictionaryWithObject:event forKey:UIKeyCommandKeyCommandEvent] alternates:alternates];
 }
 
+/**
+ UIResponder 子类只需要声明这个方法即可
+ 
+ 递归式分发键盘快捷键事件 业务请复写,返回YES 将响应链自动断掉
+-(BOOL)onKeyCommand:(UIKeyCommand *)command commandEvent:(NSString*)event{
+   NSLog(@"===========>执行key %@ by %@  %@",command.input,self,event);
+   return YES;
+}
+ */
 + (instancetype)dispatchKeyCommandWithInput:(NSString *)input modifierFlags:(UIKeyModifierFlags)modifierFlags commandEvent:(NSString *) event{
     return [UIKeyCommand commandWithTitle:@"" image:nil action: [self getDefaulActionSelector] input:input modifierFlags:modifierFlags propertyList:[NSDictionary dictionaryWithObject:event forKey:UIKeyCommandKeyCommandEvent]];
 }
