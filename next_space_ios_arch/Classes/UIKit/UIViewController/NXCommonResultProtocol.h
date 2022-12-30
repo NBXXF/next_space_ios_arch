6a739709cfad196d1c36242000a0982445bcdc81
mobileflowus <mobile@flowus.cn>
2022-12-30 16:47:58 +0800

    feat: ViewController 支持路由参数

---
+++ next_space_ios_arch/Classes/UIKit/UIViewController/NXCommonResultProtocol.h
@@ -0,0 +1,22 @@
+//
+//  NXCommonResultProtocol.h
+//  NXRouter
+//
+//  Created by XXF on 2022/7/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+
+@protocol NXCommonResultProtocol <NSObject>
+
+/// 方法协议调用
+-(void)setBaseResult:(id)result;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
