95b33011e84639fc79a6e3cc92dd486c1c9a223e
mobileflowus <mobile@flowus.cn>
2022-12-31 21:46:28 +0800

    feat: 主线程性能监控

---
+++ next_space_ios_arch/Classes/Performance/BlockWatcher.swift
@@ -0,0 +1,84 @@
+import Foundation
+
+//  BlockWatcher.swift
+//  AFNetworking
+//  监控卡顿
+//  Created by XXF on 2022/12/31.
+//
+final public class BlockWatcher: NSObject {
+    fileprivate let pingThread: WatchThread
+
+    @objc public static let defaultThreshold = 0.4
+
+    /// - parameter threshold: 卡顿时间 单位为s.
+    /// - parameter strictMode: 区分是打印日志还是报错.
+    @objc public convenience init(threshold: Double = BlockWatcher.defaultThreshold, strictMode: Bool = false) {
+        let message = "👮 Main thread was blocked for " + String(format: "%.2f", threshold) + "s 👮"
+
+        self.init(threshold: threshold) {
+            if strictMode {
+                //主线程卡顿了 请用xcode 切换Thread 查看
+                fatalError(message)
+            } else {
+                NSLog("%@", message)
+            }
+        }
+    }
+
+    
+    @objc public init(threshold: Double = BlockWatcher.defaultThreshold, watchdogFiredCallback: @escaping () -> Void) {
+        self.pingThread = WatchThread(threshold: threshold, handler: watchdogFiredCallback)
+
+        self.pingThread.start()
+        super.init()
+    }
+    
+    deinit {
+        pingThread.cancel()
+    }
+}
+
+private final class WatchThread: Thread {
+    fileprivate var pingTaskIsRunning: Bool {
+        get {
+            objc_sync_enter(pingTaskIsRunningLock)
+            let result = _pingTaskIsRunning;
+            objc_sync_exit(pingTaskIsRunningLock)
+            return result
+        }
+        set {
+            objc_sync_enter(pingTaskIsRunningLock)
+            _pingTaskIsRunning = newValue
+            objc_sync_exit(pingTaskIsRunningLock)
+        }
+    }
+    private var _pingTaskIsRunning = false
+    private let pingTaskIsRunningLock = NSObject()
+    fileprivate var semaphore = DispatchSemaphore(value: 0)
+    fileprivate let threshold: Double
+    fileprivate let handler: () -> Void
+    
+    init(threshold: Double, handler: @escaping () -> Void) {
+        self.threshold = threshold
+        self.handler = handler
+        super.init()
+        self.name = "WatchDog"
+    }
+    
+    override func main() {
+        while !isCancelled {
+            pingTaskIsRunning = true
+            DispatchQueue.main.async {
+                self.pingTaskIsRunning = false
+                self.semaphore.signal()
+            }
+            
+            Thread.sleep(forTimeInterval: threshold)
+            if pingTaskIsRunning {
+                handler()
+            }
+            
+            _ = semaphore.wait(timeout: DispatchTime.distantFuture)
+        }
+    }
+}
