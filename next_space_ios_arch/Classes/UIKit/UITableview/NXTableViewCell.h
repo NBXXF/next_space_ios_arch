a57d786958083c2c6a9033c0ef87e23aa392326f
mobileflowus <mobile@flowus.cn>
2023-02-09 15:58:41 +0800

    feat: s69  增加tableview 的适配器

---
+++ next_space_ios_arch/Classes/UIKit/UITableview/NXTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  NXCollectionCellView.h
+//  next_space_ios_arch
+//  先规范
+//  Created by XXF on 2022/11/18.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NXTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ next_space_ios_arch/Classes/UIKit/UITableview/NXTableViewCell.m
@@ -0,0 +1,12 @@
+//
+//  NXCollectionCellView.h
+//  next_space_ios_arch
+//  先规范
+//  Created by XXF on 2022/11/18.
+//
+
+#import "NXTableViewCell.h"
+
+@implementation NXTableViewCell
+
+@end
