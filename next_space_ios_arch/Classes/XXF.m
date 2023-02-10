b66ffed70295c4e4cd82cdddcf5f8cfc11f0e1b1
mobileflowus <mobile@flowus.cn>
2023-02-10 17:30:36 +0800

    feat: s69 推进卡顿监控 时间定位0.4s

--- next_space_ios_arch/Classes/XXF.m
+++ next_space_ios_arch/Classes/XXF.m
@@ -66,8 +66,8 @@ static BlockWatcher *__watchdog;
 
 + (void)_initPerformanceMonitor{
 #if DEBUG
-    //200ms==0.2s 业务可以选择unlock方式修改 短暂解决临时不变
-    CGFloat threshold=0.2;
+    //业务可以选择unlock方式修改 短暂解决临时不变
+    CGFloat threshold=0.4;
     if(!__watchdog){
         //单位是s 秒
         __watchdog=[[BlockWatcher alloc] initWithThreshold:threshold strictMode:YES];
