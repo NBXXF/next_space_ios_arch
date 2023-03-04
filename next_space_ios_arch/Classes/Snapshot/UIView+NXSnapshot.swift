12806fdf3d90d9cce421053be31c88c78e252131
mobileflowus <mobile@flowus.cn>
2023-03-04 20:08:49 +0800

    feat: s73 解决截图框架背景问题

--- next_space_ios_arch/Classes/Snapshot/UIView+NXSnapshot.swift
+++ next_space_ios_arch/Classes/Snapshot/UIView+NXSnapshot.swift
@@ -38,22 +38,46 @@ extension UIView: NXSnapshotKitProtocol {
     }
     
     
+    /**
+     原始代码
+     public func takeSnapshotOfFullContent(for croppingRect: CGRect) -> UIImage? {
+            // floor(x): 如果参数是小数，则求最大的整数但不大于本身，如floor(3.001)=3.0
+            // 由于 croppingView（要裁剪的子视图）的实际size，可能比获取到的size小（double精度问题导致这种差异），所以使用floor对获取到的size进行一次处理，使获取到的size不大于实际的size，以避免生成的图片底部会出现1px左右的黑线
+            var backgroundColor = self.backgroundColor ?? UIColor.white
+            // 若View为非透明且无圆角，则创建非透明的画布
+            // 非透明的画布渲染速度比透明的画布要快
+            let opaqueCanvas = (self.isOpaque && self.layer.cornerRadius == 0)
+            // 对于透明的画布，使用白色作为底色
+            if opaqueCanvas == false {
+                backgroundColor = UIColor.white
+            }
+            let contentSize = CGSize.init(width: floor(croppingRect.size.width), height: floor(croppingRect.size.height))
+            UIGraphicsBeginImageContextWithOptions(contentSize, opaqueCanvas, 0)
+
+            guard let context = UIGraphicsGetCurrentContext() else {
+                return nil
+            }
+
+            context.saveGState()
+            context.clear(croppingRect)
+            context.setFillColor(backgroundColor.cgColor)
+            context.fill(croppingRect)
+            context.translateBy(x: -croppingRect.origin.x, y: -croppingRect.origin.y)
+            self.layer.render(in: context)
+            context.restoreGState()
+
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+
+            return image
+        }
+     */
     @objc
     public func nx_takeSnapshotOfFullContent(for croppingRect: CGRect,bgColor:UIColor) -> UIImage? {
-        // floor(x): 如果参数是小数，则求最大的整数但不大于本身，如floor(3.001)=3.0
-        // 由于 croppingView（要裁剪的子视图）的实际size，可能比获取到的size小（double精度问题导致这种差异），所以使用floor对获取到的size进行一次处理，使获取到的size不大于实际的size，以避免生成的图片底部会出现1px左右的黑线
-//        var backgroundColor = self.backgroundColor ?? UIColor.white //update by XXF
         var backgroundColor = bgColor;
-        // 若View为非透明且无圆角，则创建非透明的画布
-        // 非透明的画布渲染速度比透明的画布要快
-        let opaqueCanvas = (self.isOpaque && self.layer.cornerRadius == 0)
-        // 对于透明的画布，使用白色作为底色
-        if opaqueCanvas == false {
-           // backgroundColor = UIColor.white //update by XXF
-            backgroundColor = bgColor;
-        }
         let contentSize = CGSize.init(width: floor(croppingRect.size.width), height: floor(croppingRect.size.height))
-        UIGraphicsBeginImageContextWithOptions(contentSize, opaqueCanvas, 0)
+        //第二个参数需要传false by xxf
+        UIGraphicsBeginImageContextWithOptions(contentSize, false, 0)
 
         guard let context = UIGraphicsGetCurrentContext() else {
             return nil
@@ -69,7 +93,7 @@ extension UIView: NXSnapshotKitProtocol {
 
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-
+        
         return image
     }
 }
