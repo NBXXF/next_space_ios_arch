2b6d426e17ca4e2ce13a053920c08143b64f4c7a
mobileflowus <mobile@flowus.cn>
2023-03-04 15:02:16 +0800

    feat: s73 截图格式支持JPEG

--- next_space_ios_arch/Classes/UIKit/UIImage/UIImage+NXTools.h
+++ next_space_ios_arch/Classes/UIKit/UIImage/UIImage+NXTools.h
@@ -15,13 +15,32 @@ NS_ASSUME_NONNULL_BEGIN
  将图片保存到本地
  图片保存本地
  */
-- (BOOL)writePNGToFile:(NSString *)path atomically:(BOOL)useAuxiliaryFile;
-- (BOOL)writePNGToFile:(NSString *)path options:(NSDataWritingOptions)writeOptionsMask error:(NSError **)errorPtr;
+- (BOOL)writePNGToFile:(NSString *)path
+            atomically:(BOOL)useAuxiliaryFile;
+- (BOOL)writePNGToFile:(NSString *)path
+               options:(NSDataWritingOptions)writeOptionsMask
+                 error:(NSError **)errorPtr;
+
+- (BOOL)writeJPEGToFile:(NSString *)path
+             atomically:(BOOL)useAuxiliaryFile;
+- (BOOL)writeJPEGToFile:(NSString *)path
+             atomically:(BOOL)useAuxiliaryFile
+     compressionQuality:(CGFloat)compressionQuality;
+- (BOOL)writeJPEGToFile:(NSString *)path
+                options:(NSDataWritingOptions)writeOptionsMask
+                  error:(NSError **)errorPtr;
+- (BOOL)writeJPEGToFile:(NSString *)path
+                options:(NSDataWritingOptions)writeOptionsMask
+                  error:(NSError **)errorPtr
+     compressionQuality:(CGFloat)compressionQuality;
 
 /**
  转data
  */
 -(NSData *)PNGData;
+
+-(NSData *)JPEGData;
+-(NSData *)JPEGDataWithQuality:(CGFloat)compressionQuality;
 @end
 
 NS_ASSUME_NONNULL_END
--- next_space_ios_arch/Classes/UIKit/UIImage/UIImage+NXTools.m
+++ next_space_ios_arch/Classes/UIKit/UIImage/UIImage+NXTools.m
@@ -9,15 +9,66 @@
 
 @implementation UIImage(NXFile)
 
-- (BOOL)writePNGToFile:(NSString *)path atomically:(BOOL)useAuxiliaryFile{
-    BOOL result = [UIImagePNGRepresentation(self) writeToFile:path atomically:YES];
+- (BOOL)writePNGToFile:(NSString *)path
+            atomically:(BOOL)useAuxiliaryFile{
+    BOOL result = [self.PNGData writeToFile:path
+                                                   atomically:YES];
     return result;
 }
-- (BOOL)writePNGToFile:(NSString *)path options:(NSDataWritingOptions)writeOptionsMask error:(NSError *__autoreleasing  _Nullable *)errorPtr{
-    BOOL result = [UIImagePNGRepresentation(self) writeToFile:path options:writeOptionsMask error:errorPtr];
+- (BOOL)writePNGToFile:(NSString *)path
+               options:(NSDataWritingOptions)writeOptionsMask
+                 error:(NSError *__autoreleasing  _Nullable *)errorPtr{
+    BOOL result = [self.PNGData writeToFile:path
+                                                      options:writeOptionsMask
+                                                        error:errorPtr];
     return result;
 }
+
+- (BOOL)writeJPEGToFile:(NSString *)path
+             atomically:(BOOL)useAuxiliaryFile{
+    return [self writeJPEGToFile:path
+                      atomically:useAuxiliaryFile
+              compressionQuality:1];
+}
+
+- (BOOL)writeJPEGToFile:(NSString *)path
+             atomically:(BOOL)useAuxiliaryFile
+     compressionQuality:(CGFloat)compressionQuality
+{
+    BOOL result = [[self JPEGDataWithQuality:compressionQuality] writeToFile:path
+                                                                        atomically:YES];
+    return result;
+}
+
+- (BOOL)writeJPEGToFile:(NSString *)path
+                options:(NSDataWritingOptions)writeOptionsMask
+                  error:(NSError *__autoreleasing  _Nullable *)errorPtr{
+    return [self writeJPEGToFile:path
+                         options:writeOptionsMask
+                           error:errorPtr
+              compressionQuality:1];
+}
+
+- (BOOL)writeJPEGToFile:(NSString *)path
+                options:(NSDataWritingOptions)writeOptionsMask
+                  error:(NSError *__autoreleasing  _Nullable *)errorPtr
+     compressionQuality:(CGFloat)compressionQuality{
+    BOOL result = [[self JPEGDataWithQuality:compressionQuality] writeToFile:path
+                                                                           options:writeOptionsMask
+                                                                             error:errorPtr];
+    return result;
+}
+
 - (NSData *)PNGData{
     return UIImagePNGRepresentation(self);
 }
+
+- (NSData *)JPEGData{
+    return [self JPEGDataWithQuality:1];
+}
+- (NSData *)JPEGDataWithQuality:(CGFloat)compressionQuality{
+    return UIImageJPEGRepresentation(self, compressionQuality);
+}
+
+
 @end
