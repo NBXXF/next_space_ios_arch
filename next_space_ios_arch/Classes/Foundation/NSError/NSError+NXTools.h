7ceb31fd6b84e6558187dcbf327eaee8ab610617
mobileflowus <mobile@flowus.cn>
2023-03-14 17:47:50 +0800

    feat:s73 增加全能的try cactch

--- next_space_ios_arch/Classes/Foundation/NSError/NSError+NXTools.h
+++ next_space_ios_arch/Classes/Foundation/NSError/NSError+NXTools.h
@@ -9,6 +9,25 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSError(NXTools)
+/**
+ 比较万能,业务上下游不确定的时候 还有框架保险机制
+ 1.throw的error  try catch(NSException *) 生效么？
+ 2.throw的exception  try catch(NSError *) 生效么？
+ 3.throw的exception或者error  try catch(id *) 生效么？
+ 答案是NSError 和NSException没任何关系,如果写了具体的类型 类型对不上反而无效
+ 1.无效
+ 2.无效
+ 3.都有效
+ 
+ 内部支持将NSException 和NSError转换成 NSError
+ 
+ 用法 eg.
+ @catch (NSObject *exception) {
+    NSError *error=[NSError nx_ErrorWithErrorOrException:exception];
+ }
+ */
++ (NSError *)nx_ErrorWithErrorOrException:(NSObject *)errorOrException;
+
 + (NSError *)nx_ErrorWithCode:(NSInteger)code text:(NSString *)text;
 
 + (NSError *)nx_ErrorWithError:(NSError *)error userInfo:(NSDictionary *)userInfo;
