0515b9296034a322ed196098cdd115b73eafa539
mobileflowus <mobile@flowus.cn>
2023-03-15 22:03:32 +0800

    feat:s74 支持类方法交换

--- next_space_ios_arch/Classes/NSObject/NSObject+Swizzling.h
+++ next_space_ios_arch/Classes/NSObject/NSObject+Swizzling.h
@@ -9,7 +9,15 @@
 #import <Foundation/Foundation.h>
 
 @interface NSObject (Swizzling)
-+ (void)exchangeInstanceMethodWithSelfClass:(Class)selfClass
++ (void)exchangeInstanceMethodWithSelfClass:(Class)cls
+                           originalSelector:(SEL)originalSelector
+                           swizzledSelector:(SEL)swizzledSelector DEPRECATED_MSG_ATTRIBUTE("请使用nx_swizzleInstanceMethodWithClass");
+
++ (void)nx_swizzleInstanceMethodWithClass:(Class)cls
+                           originalSelector:(SEL)originalSelector
+                           swizzledSelector:(SEL)swizzledSelector;
+
++ (void)nx_swizzleClassMethodWithClass:(Class)cls
                            originalSelector:(SEL)originalSelector
                            swizzledSelector:(SEL)swizzledSelector;
 @end
--- next_space_ios_arch/Classes/NSObject/NSObject+Swizzling.m
+++ next_space_ios_arch/Classes/NSObject/NSObject+Swizzling.m
@@ -10,23 +10,65 @@
 #import "NSObject+Swizzling.h"
 
 @implementation NSObject (Swizzling)
-+ (void)exchangeInstanceMethodWithSelfClass:(Class)selfClass
++ (void)exchangeInstanceMethodWithSelfClass:(Class)cls
                            originalSelector:(SEL)originalSelector
                            swizzledSelector:(SEL)swizzledSelector {
     
-    Method originalMethod = class_getInstanceMethod(selfClass, originalSelector);
-    Method swizzledMethod = class_getInstanceMethod(selfClass, swizzledSelector);
-    BOOL didAddMethod = class_addMethod(selfClass,
+    return [self nx_swizzleInstanceMethodWithClass:cls
+                                  originalSelector:originalSelector
+                                  swizzledSelector:swizzledSelector];
+}
+
+
+
++ (void)nx_swizzleInstanceMethodWithClass:(Class)cls
+                         originalSelector:(SEL)originalSelector
+                         swizzledSelector:(SEL)swizzledSelector{
+    Method originalMethod = class_getInstanceMethod(cls, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(cls, swizzledSelector);
+    BOOL didAddMethod = class_addMethod(cls,
                                         originalSelector,
                                         method_getImplementation(swizzledMethod),
                                         method_getTypeEncoding(swizzledMethod));
     if (didAddMethod) {
-        class_replaceMethod(selfClass,
+        class_replaceMethod(cls,
                             swizzledSelector,
                             method_getImplementation(originalMethod),
                             method_getTypeEncoding(originalMethod));
     } else {
         method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+}
+
++ (void)nx_swizzleClassMethodWithClass:(Class)cls
+                      originalSelector:(SEL)originalSelector
+                      swizzledSelector:(SEL)swizzledSelector{
+    if (!cls) {
+        return;
+    }
+    Method originalMethod = class_getClassMethod(cls, originalSelector);
+    Method swizzledMethod = class_getClassMethod(cls, swizzledSelector);
+    
+    Class metacls = objc_getMetaClass(NSStringFromClass(cls).UTF8String);
+    if (class_addMethod(metacls,
+                        originalSelector,
+                        method_getImplementation(swizzledMethod),
+                        method_getTypeEncoding(swizzledMethod)) ) {
+        /* swizzing super class method, added if not exist */
+        class_replaceMethod(metacls,
+                            swizzledSelector,
+                            method_getImplementation(originalMethod),
+                            method_getTypeEncoding(originalMethod));
+        
+    } else {
+        /* swizzleMethod maybe belong to super */
+        class_replaceMethod(metacls,
+                            swizzledSelector,
+                            class_replaceMethod(metacls,
+                                                originalSelector,
+                                                method_getImplementation(swizzledMethod),
+                                                method_getTypeEncoding(swizzledMethod)),
+                            method_getTypeEncoding(originalMethod));
     }
 }
 
