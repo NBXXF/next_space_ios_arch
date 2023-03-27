57a80088d2e583a64799c66b44e2263fecae34a6
mobileflowus <mobile@flowus.cn>
2023-03-27 16:43:22 +0800

    feat:s76  UITableViewCell和UICollectionView 去除默认动画有bug

--- next_space_ios_arch/Classes/UIKit/Animation/UIControl+Animation.m
+++ next_space_ios_arch/Classes/UIKit/Animation/UIControl+Animation.m
@@ -8,8 +8,10 @@
 #import "UIControl+Animation.h"
 #import <next_space_ios_arch/UIView+Animation.h>
 #import <next_space_ios_arch/NSObject+Swizzling.h>
+#import <next_space_ios_arch/NSObject+NXAssociation.h>
 
 @implementation UIControl(Animation)
+
 
 + (void)load{
     static dispatch_once_t onceToken;
@@ -20,9 +22,23 @@
     });
 }
 
+-(NSString *)_keyAllowAnimationForHighlight{
+    return NSStringFromSelector(@selector(allowAnimationForHighlight));
+}
+- (BOOL)allowAnimationForHighlight{
+    //默认YES
+    id result=[self nx_getAssociatedObject:self._keyAllowAnimationForHighlight]?:@(YES);
+    return [result boolValue];
+}
+
+- (void)setAllowAnimationForHighlight:(BOOL)allowAnimationForHighlight{
+    [self nx_setAssociatedObject:@(allowAnimationForHighlight)
+                          forKey:self._keyAllowAnimationForHighlight
+                          policy:NX_ASSOCIATION_ASSIGN];
+}
 
 - (void)_hook_animation_setHighlighted:(BOOL)highlighted{
-    if(highlighted){
+    if(highlighted&&self.allowAnimationForHighlight){
         [self animateWithBounce];
     }
     [self _hook_animation_setHighlighted:highlighted];
